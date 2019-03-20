8dd2c91dcd9c979f0beafc6f0bcf9608b6dd1f57
Nicholas Livingstone <freezydrag@gmail.com>
2019-03-20 13:17:25 -0700

    Created Sorted Linked List Assignment

---
+++ Sorted Linked List/src/Node.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author freez
+ */
+public class Node<T> {
+    private <T> data;
+    private <T> adress; 
+}
