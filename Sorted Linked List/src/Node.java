5b0b20a8f737aaa3df55536189b305aaa6694188
Nicholas Livingstone <freezydrag@gmail.com>
2019-03-20 20:53:53 -0700

    Worked on Sorted Linked List

--- Sorted Linked List/src/Node.java
+++ Sorted Linked List/src/Node.java
@@ -9,6 +9,46 @@
  * @author freez
  */
 public class Node<T> {
-    private <T> data;
-    private <T> adress; 
+    private T data;
+    private Node<T> next; 
+    private Node<T> prev;
+
+    public Node(){
+        this.prev = null;
+        this.data = null; 
+        this.next = null;
+    }
+
+    public Node(T data){
+        this.prev = null;
+        this.data = data; 
+        this.next = null;
+    }
+
+    public Node(T data, Node<T> prev, Node<T> next){
+        this.prev = prev;
+        this.data = data; 
+        this.next = next;
+    }
+
+
+    public T getData(){
+        return data; 
+    }
+
+    public Node<T> getNext(){
+        return next; 
+    }
+
+    public void setNext(Node<T> next){
+        this.next = next; 
+    }
+
+    public Node<T> getPrev(){
+        return prev; 
+    }
+
+    public void setPrev(Node<T> prev){
+        this.prev = prev; 
+    }
 }
---
+++ Sorted Linked List/src/SortedLinkedList.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author freez
+ */
+public class SortedLinkedList<T> {
+    private Node head;
+    private int numNodes;
+
+    public SortedLinkedList(Node<T> node){
+        this.head = node; 
+    }
+
+    public void addNode(Node<T> newNode){
+        boolean cont = true; 
+        Node tmp = head; 
+
+        do{
+            if(newNode.getData() < tmp.getData()){
+                newNode.setNext(tmp);
+                newNode.setPrev(tmp.getPrev());
+                tmp.setPrev(newNode);
+                cont = false; 
+            }
+            else if(tmp.getNext() == null){
+                tmp.setNext(newNode);
+                newNode.setPrev(tmp);
+                cont = false; 
+            }
+            else{
+                tmp = head.getNext();
+            }
+        }while(cont); 
+        
+    }
+}
