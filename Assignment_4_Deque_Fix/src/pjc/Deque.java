23be1e33b19438b4a4348b8e2a5230e6d2e368c2
Nicholas Livingstone <35415469+nl2645468@users.noreply.github.com>
2019-05-30 15:18:00 -0700

    Finished Assignmnet 4 Deque Fix

---
+++ Assignment_4_Deque_Fix/src/pjc/Deque.java
@@ -0,0 +1,195 @@
+package pjc;
+
+public class Deque<T> implements DequeInterface<T>
+{
+    // linked list containing the items in the deque
+    private Node<T> head=null, tail=null;
+
+    // current number of items in deque
+    private int numberOfItems;
+
+    // Queue constructor that starts with empty deque.
+    public Deque()
+    {
+        numberOfItems=0; // no items yet!
+    }
+	
+    // displays current contents of the deque
+    public void display()
+    {
+        Node current=head;
+        for(int i=0;i<numberOfItems;i++)
+        {
+            System.out.print(current.getValue()+" ");
+            current=current.getNext();
+        }
+    }
+	
+    // ----- functions/methods in the interface that must be implemented -----
+
+    // returns true if the deque is empty (no items in deque) 
+    // false if deque is (has at least one or more items in deque)
+    public boolean isEmpty()
+    {
+        return numberOfItems==0;
+    }
+	
+	// return the number of items currently in the deque
+    public int size()
+    {
+        return numberOfItems;
+    }
+ 
+    // returns the value of the item currently at front of deque
+    public T front()
+    {
+        if ( head==null ) return null;
+        return head.getValue(); // return item stored at head node
+    }
+	
+    // returns the value of the item currently at the end of the deque
+    public T back()
+    {
+        if ( tail==null ) return null;
+        return tail.getValue(); // return item stored at tail node
+    }
+	
+    // places parameter item onto the end of the deque
+    public void push_back(T newItem)
+    {
+        // if head node is null, make head and tail node contain the first node
+        if ( head == null)
+        {
+            head = new Node(newItem);
+            tail=head; // when first item is enqueued, head and tail are the same
+            numberOfItems++; // increment the number of items in the deque
+        }
+        else
+        {
+            Node<T> newNode = new Node(newItem);
+            tail.setNext(newNode);
+            newNode.setPrevious(tail);
+            tail=newNode;
+            numberOfItems++;
+        }
+    }
+	
+    public void enqueue_back(T newItem) // this is the same concept as push_back
+    {
+        this.push_back(newItem); // since it's the same as push_back, just call push_back
+    }
+
+    // returns and removes the current item at the front of the deque
+    // the item that is in the deque behind the item becomes the new front item
+    public T pop_front()
+    {
+	T headDataValue = null;
+	if ( numberOfItems > 0 )
+	{
+            headDataValue = head.getValue();
+            Node<T> oldHead=head;
+            head=head.getNext();
+            oldHead.setNext(null);
+            oldHead.setPrevious(null);
+
+            numberOfItems--;
+        }
+        return headDataValue;  // returns the data value from the popped head, null if deque empty
+    }
+	
+    // returns and removes the current item at the back of the deque
+    // the item that is in the deque in front of the item being removed becomes the new back item
+    public T pop_back()
+    {
+	T tailDataValue = null;
+	if ( numberOfItems > 0 )
+	{
+            tailDataValue = tail.getValue();
+            Node<T> oldTail=tail;
+            tail=tail.getPrevious();
+            tail.setNext(null);
+            oldTail.setPrevious(null);
+
+            numberOfItems--;
+        }
+        return tailDataValue;  // returns the data value from the popped tail, null if deque empty
+    }
+    
+    public T dequeue_back() // this is the same as pop_back
+    {
+        return this.pop_back();
+    }
+    
+    public T dequeue_front() // this is the same concept as pop_front
+    {
+        return this.pop_front(); // since it's the same as pop_front, just call pop_front
+    }
+	
+    public void enqueue_front(T newItem) // this is the same concept as push_front
+    {
+        this.push_front(newItem); // since it's the same as push_front, just call push_front
+    }
+    
+    // places parameter item onto the end of the deque
+    public void push_front(T newItem)
+    {
+        // if head node is null, make head and tail node contain the first node
+        if ( head == null)
+        {
+            head = new Node(newItem);
+            tail=head; // when first item is enqueued, head and tail are the same
+            numberOfItems++; // increment the number of items in the deque
+        }
+        else
+        {
+            Node<T> newNode = new Node(newItem);
+            newNode.setNext(head);
+            head=newNode;
+            head.getNext().setPrevious(head);
+            numberOfItems++;
+        }
+    }
+    
+    // Node Inner Class
+    private class Node<T> {
+        private T value;
+        private Node _previous, _next;
+
+        public Node(T data) {
+            value = data;
+            _previous = null;
+            _next = null;
+        }
+    
+        protected Node(T data, Node previousNode, Node nextNode) {
+            value = data;
+            _previous = previousNode;
+            _next = nextNode;
+        }
+		
+        public Node getNext() {
+            return _next;
+        }
+
+        public Node getPrevious() {
+            return _previous;
+        }
+
+        public void setValue(T newValue)
+        {
+            value=newValue;
+        }
+
+        public T getValue() {
+            return value;
+        }
+
+        public void setNext(Node newNextNode) {
+            _next = newNextNode;
+        }
+		
+        public void setPrevious(Node newPreviousNode) {
+            _previous = newPreviousNode;
+        }
+    }
+}
---
+++ Assignment_4_Deque_Fix/src/pjc/DequeDemo.java
@@ -0,0 +1,117 @@
+package pjc;
+
+public class DequeDemo {
+
+    public static void main(String[] args) {
+        Deque<String> words = new Deque<String>();
+        
+        // interactive testing
+        do {
+            System.out.println("\nDeque Test Menu");
+            System.out.println("1. Enqueue_front a word");
+            System.out.println("2. Enqueue_back a word");
+            System.out.println("3. Push_front a word");
+            System.out.println("4. Push_back a word");
+            System.out.println("5. Dequeue_front a word");
+            System.out.println("6. Dequeue_back a word");
+            System.out.println("7. Pop_front a word");
+            System.out.println("8. Pop_back a word");
+            System.out.println("9. Display the queue");
+            System.out.println("0. Display front and back");
+            System.out.println("Q. Quit");
+            System.out.print("Choose an option 1 to 9, 0, Q: ");
+            String choice = new java.util.Scanner(System.in).next();
+            
+            if (choice.equalsIgnoreCase("q")) break; 
+            if (choice.equals("1"))
+            {
+                System.out.print("Enter a word: ");
+                String wd = new java.util.Scanner(System.in).next();
+                
+                words.enqueue_front(wd);
+                
+                System.out.printf("The word '%s' enqueued to front %n", wd);
+                System.out.printf("Deque has %d elements%n",words.size());
+            }
+            
+            if (choice.equals("2"))
+            {
+                System.out.print("Enter a word: ");
+                String wd = new java.util.Scanner(System.in).next();
+                
+                words.enqueue_back(wd);
+                
+                System.out.printf("The word '%s' enqueued to back %n", wd);
+                System.out.printf("Deque has %d elements%n",words.size());
+            }
+            
+            if (choice.equals("3"))
+            {
+                System.out.print("Enter a word: ");
+                String wd = new java.util.Scanner(System.in).next();
+                
+                words.push_front(wd);
+                
+                System.out.printf("The word '%s' pushed to front %n", wd);
+                System.out.printf("Deque has %d elements%n",words.size());
+            }
+            
+            if (choice.equals("4"))
+            {
+                System.out.print("Enter a word: ");
+                String wd = new java.util.Scanner(System.in).next();
+                
+                words.push_front(wd);
+                
+                System.out.printf("The word '%s' pushed to back %n", wd);
+                System.out.printf("Deque has %d elements%n",words.size());
+            }
+        
+            if (choice.equals("5"))
+            {
+                String wd = words.dequeue_front();
+                
+                System.out.printf("The word '%s' dequeued from the front%n", wd);
+                System.out.printf("Deque has %d elements%n",words.size());
+            }
+            
+            if (choice.equals("6"))
+            {
+                String wd = words.dequeue_back();
+                
+                System.out.printf("The word '%s' dequeued from the back%n", wd);
+                System.out.printf("Deque has %d elements%n",words.size());
+            }
+            
+            if (choice.equals("7"))
+            {
+                String wd = words.pop_front();
+                
+                System.out.printf("The word '%s' popped from the front%n", wd);
+                System.out.printf("Deque has %d elements%n",words.size());
+            }
+            
+            if (choice.equals("8"))
+            {
+                String wd = words.pop_back();
+                
+                System.out.printf("The word '%s' popped from the back%n", wd);
+                System.out.printf("Deque has %d elements%n",words.size());
+            }
+            
+            
+            if (choice.equals("9")) 
+            {
+                System.out.print("The deque of words is: ");
+                words.display();
+                System.out.println();
+            }
+            
+            if (choice.equals("0"))
+            {
+                System.out.printf("front='%s', back='%s'%n",words.front(),words.back());
+            }
+        } while(true);
+    }
+    
+}
